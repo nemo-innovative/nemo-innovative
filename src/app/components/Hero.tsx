e8f6b4cbb9e0e393f8adea85bb97bec8af60c08f
SatkarNiraula <satkarniraula69@gmail.com>
2024-04-07 22:14:05 +0545

    hero image and gradient

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,15 +1,32 @@
 const Hero = () => {
   return (
     <section
-      className="body-font bg-cover bg-no-repeat bg-center flex justify-between"
-      style={{
-        backgroundImage:
-          "url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cf3c8e3b-3d47-474b-8d26-fbddddf63586/dg6n309-6133d487-9aca-40f1-9a66-e7c2efe47ccc.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2NmM2M4ZTNiLTNkNDctNDc0Yi04ZDI2LWZiZGRkZGY2MzU4NlwvZGc2bjMwOS02MTMzZDQ4Ny05YWNhLTQwZjEtOWE2Ni1lN2MyZWZlNDdjY2MuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.h_JTc7m0-fxoIY5hkORs2K7EQFSLLzyTNSYQdwDKlqU)",
-        backgroundSize: "cover",
-        backgroundPosition: "right",
-      }}
+      className="w-full
+    h-screen
+    max-h-[32rem]
+    p-24
+    relative
+    overflow-hidden
+    block
+    z-10
+    
+    bg-[url('https://media.discordapp.net/attachments/1222590290024730695/1226558174824239225/istockphoto-513247652-612x612_upscayl_4x_realesrgan-x4plus.png?ex=66253451&is=6612bf51&hm=1a2876cf56d02a49c034de9c9207396f4c6f7324916e07de0599fb377676bddf&=&format=webp&quality=lossless&width=1146&height=662')]
+    bg-cover
+    bg-no-repeat
+    bg-center
+
+    before:content-['']
+    before:absolute
+    before:inset-0
+    before:block
+    before:bg-gradient-to-r
+    before:from-blue-500
+    before:to-green-400
+    before:opacity-60
+    before:z-[-5]
+"
     >
-      <div className="container px-5 py-40 mx-auto w-full max-w-screen-xl">
+      <div className="container px-5  mx-auto w-full max-w-screen-xl">
         <div className="flex flex-wrap -m-12">
           <div className="p-12 md:w-1/2 flex flex-col items-start">
             <h2 className="text-2xl sm:text-4xl title-font font-medium text-white mt-24 mb-7 hover:text-[#0d7abf]">
